4f50f698540af2a2cb7b2dbf8e2a716f0396b078
Osama Jouda <oj.turk@gmail.com>
2022-10-11 13:07:55 +0300

    defining application middlewares

--- app/Http/Middleware/CheckPassword.php
+++ app/Http/Middleware/CheckPassword.php
@@ -3,19 +3,29 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class CheckPassword
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(Request): (JsonResponse|RedirectResponse) $next
+     * @return JsonResponse|RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next):JsonResponse|RedirectResponse
     {
+        if(!isset($request->password) || $request->password !== env('PASSWORD','DEFAULT_PASSWORD'))
+        {
+            return response()->json([
+                'status'=>false,
+                'code'=>'E001',
+                'msg'=>'Unauthenticated. ',
+            ]);
+        }
         return $next($request);
     }
 }
